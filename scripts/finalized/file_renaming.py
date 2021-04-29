01fe9e835260e3fa146c8d36d4f30e0ebe850194
Holly Paden <paden.10@osu.edu>
2021-04-29 15:40:38 -0400

    updated Final_Documentation_README.md with directions on installing the ipy-env.  Updated file_renaming.py with  command to make it more verbose in case of errors.  Moved slurm_profile directory to workflow directory.  Updated Process_README.md with details

--- scripts/finalized/file_renaming.py
+++ scripts/finalized/file_renaming.py
@@ -10,7 +10,7 @@ import sys
 # generate the diet array in a 15 row by 8 column pattern with 120 numbers beginning at 1
 print("Generating diet array and assigning diets")
 diet_array = np.arange(1, 121).reshape((15, 8))
-diet_array
+print(diet_array)
 #%%
 # convert array to string so that floating zeros can be used to make all numbers 3 digits long
 diet_array = diet_array.astype(str)
@@ -18,16 +18,16 @@ np.char.zfill(diet_array, 3)
 #%%
 # Assign Diets A-D with 2 columns per diet
 A = np.char.zfill(diet_array[ :, 0:2], 3)
-A
+print(A)
 # %%
 B = np.char.zfill(diet_array[ :, 2:4], 3)
-B
+print(B)
 # %%
 C = np.char.zfill(diet_array[ :, 4:6], 3)
-C
+print(C)
 # %%
 D = np.char.zfill(diet_array[ :, 6:8], 3)
-D
+print(D)
 #%%
 # determine relative path and strip to basename
 input=sys.argv[1]
@@ -39,20 +39,19 @@ print(basename)
 # convert basename to a string and then split into a list by the delimiter "_"
 print("Begin renaming process, please wait...")
 seq1_str = str(basename)
-seq1_str
+print(seq1_str)
 # %%
 seq_list = seq1_str.split("_")
-seq_list
+print(seq_list)
 #%%
 # assign "day" as the first item in the list, then insert "ay" between "D" and "##"
 day = seq_list[0]
 #%%
 day = day[:1] + "ay" + day[1:]
-day
+print(day)
 #%%
 # assign "sample" first and then use it to determine "diet" before further editing "sample"
 sample = seq_list[1]
-sample
 #%%
 # use the "diet" array and an `if` loop to determine diet based on sample ###
 diet = seq_list[3]
@@ -66,24 +65,24 @@ elif sample in C:
 elif sample in D:
     diet = "DietD"
 
-diet
+print(diet)
 #%%
 # now add the word "saample" in front of our sample ##
 sample = "Sample" + sample[:]
-sample
+print(sample)
 #%%
 # assign "treatment" to the third item in the list and add the word "Treatment" in front of it
 treatment = seq_list[2]
 #%%
 treatment = "Treatment" + treatment[:]
-treatment
+print(treatment)
 #%%
 # create the new name by assembling "day", "sample", "diet", and "treatment" in order, separated by "_" delimiter
 # follow "treatment" with the 6th list item, which shows if the fastq is forward or reverse
 # end by adding "fastq.gz" to the end of the file name
 new_name = day + '_' + sample + '_' + diet  + '_' + treatment + '_' + seq_list[5] + '.fastq.gz'
 print("Renaming complete...")
-new_name
+print(new_name)
 # create a complete new name with path
 full_new_name = "output_dir"+'/'+"renamed_fastq"+'/'+new_name
 #%%
